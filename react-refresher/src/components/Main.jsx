28827e6f0423a075acacb56c15853a5e8b173687
Chaitanya-963 <chaitanyamehetre0301@gmail.com>
2026-05-02 14:20:05 +0530

    React-refresher for Mobile development

---
+++ react-refresher/src/components/Main.jsx
@@ -0,0 +1,7 @@
+const Main = () => {
+  return (
+    <div>Hello this is the main section</div>
+  )
+}
+
+export default Main
---
+++ react-refresher/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import Main from "./components/Main";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <h1>Hello Chaitanya!..</h1>
+    <Main />
+  </>,
+);
